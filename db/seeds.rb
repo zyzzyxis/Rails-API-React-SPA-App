48ef87f7ad48900e6dc47f54add6c5b322720ea2
zyzzyxis <13ezimm@gmail.com>
2021-03-08 14:53:09 -0700

    faker working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require "faker"
+
+Task.destroy_all
+
+
+10.times do
+  Task.create(
+    # x = rand(1...12),
+    # y = rand(1...31),
+    # z = rand(1970...2000),
+    title: Faker::Job.key_skill,
+    description: Faker::Movies::HitchhikersGuideToTheGalaxy.quote,
+    duedate: Faker::Date.in_date_period
+  )
+end 
+puts "seeded #{Task.all.size}"
