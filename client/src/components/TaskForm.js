e6b43fee234fe0919510ada3a1664cac7825634a
ethan.mcneal <ethan.mcneal@gmail.com>
2021-03-08 16:06:40 -0700

    form rendered

---
+++ client/src/components/TaskForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+ 
+
+const TaskForm = () => {
+    return(
+        <form>
+            <h1>New Task Form</h1>
+            <p>Title</p>
+            <input />
+            <p>Description</p>
+            <input />
+            <p>Due Date</p>
+            <input />
+        </form>
+    )
+};
+
+
+export default TaskForm
